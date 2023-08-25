68982331f769727100153205e13f64f8efab931d
刘林 <liul@yxt.com>
2023-08-25 11:16:43 +0800

    ddd

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import uvicorn
 from config import settings
 from api.api import api_router
 
+# FastAPI实例
 app = FastAPI(
     debug=settings.APP_DEBUG,
     title=settings.PROJECT_NAME,
     version=settings.VERSION
 )
 
-# 路由
+# 添加路由
 app.include_router(api_router)
 
+# 启动
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True, port=9527)
